83a3bbcc19ae4cb7ad13e645ee7283bcfbc994ba
blackburn <matthewalexanderb@gmail.com>
2024-04-30 21:11:16 -0400

    added multi-headed functionality, feed-forward layers, layer-normalization

--- model.py
+++ model.py
@@ -5,16 +5,19 @@ import torch.nn.functional as F
 
 # hyperparameters
 batch_size = 32
-block_size = 8
-iterations = 30000
-eval_interval = 300
-learning_rate = 1e-2
+block_size = 64
+iterations = 5000
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embed = 32
+eval_iters = 200 
+n_embed = 128
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 # open training data
-with open('./data/input.txt', 'r', encoding='utf-8') as f:
+with open('./data/cleanelmos.txt', 'r', encoding='utf-8') as f:
 	text = f.read()
 
 # get vocab
@@ -58,21 +61,71 @@ def estimate_loss():
 	model.train()
 	return out
 
+class FeedForward(nn.Module):
+  # linear feed forward w/ reLU
+  def __init__(self, n_embed):
+    super().__init__()
+    self.net = nn.Sequential(
+      nn.Linear(n_embed, 4 * n_embed),
+      nn.ReLU(),
+      nn.Linear(4 * n_embed, n_embed),
+      nn.Dropout(dropout)
+    )
+    
+  def forward(self, x):
+    return self.net(x)
+  
+  
+class Block(nn.Module):
+  def __init__(self, n_embd, n_head):
+    super().__init__()
+    head_size = n_embd // n_head
+    self.sa = MultiHeadAttention(n_head, head_size)
+    self.ffwd = FeedForward(n_embd)
+    self.ln1 = nn.LayerNorm(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd)
+  
+  def forward(self, x):
+    x = x + self.sa(self.ln1(x))
+    x = x + self.ffwd(self.ln2(x))
+    return x
+
+class LayerNorm1d:
+  def __init__(self, dim, eps=1e-5, momentum=0.1):
+    self.eps = eps
+    
+    # parameters
+    self.gamma = torch.ones(dim)
+    self.beta = torch.zeros(dim)
+    
+  def call(self, x):
+    xmean = x.mean(1, keepdim=True)
+    xvar = x.var(1, keepdim=True)
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+    self.out = self.gamma * xhat + self.beta
+    return self.out
+  
+  def parameters(self):
+    return [self.gamma, self.beta]
 
 # bigram model
-class BigramLanguageModel(nn.Module):
+class TransformerLanguageModel(nn.Module):
 	
   def __init__(self):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
     self.position_embedding_table = nn.Embedding(block_size, n_embed)
-    self.lm_head = nn.Linear(n_embed)
+    self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layer)])
+    self.ln_f = nn.LayerNorm(n_embed)
+    self.lm_head = nn.Linear(n_embed, vocab_size)
     
   def forward(self, idx, targets=None):
     B, T = idx.shape
     tok_emb = self.token_embedding_table(idx) # (B, T, C)
-    pos_emb = self.token_embedding_table(torch.arrange(T, device=device)) # (T, C)
+    pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
     x = tok_emb + pos_emb # (B, T, C)
+    x = self.blocks(x) # apply head of self-attention
+    x = self.ln_f(x)
     logits = self.lm_head(x) # (B, T, vocab_size)
 
 
@@ -88,7 +141,8 @@ class BigramLanguageModel(nn.Module):
 
   def generate(self, idx, max_new_tokens):
     for _ in range(max_new_tokens):
-      logits, loss = self(idx)
+      idx_cond = idx[:, -block_size:]
+      logits, loss = self(idx_cond)
       logits = logits[:, -1, :]
       probs = F.softmax(logits, dim=-1)
       idx_next = torch.multinomial(probs, num_samples=1)
@@ -105,6 +159,8 @@ class Head(nn.Module):
     self.value = nn.Linear(n_embed, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
     
+    self.dropout = nn.Dropout(dropout)
+    
   def forward(self, x):
     B,T,C = x.shape      
     k = self.key(x)      # (B, T, C)
@@ -113,14 +169,27 @@ class Head(nn.Module):
     wei = q @ k.transpose(-2, -1) * C ** -0.5 # (B, T, T)
     wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) # (B, T, T)
     wei = F.softmax(wei, dim=-1) # (B, T, T)
+    wei = self.dropout(wei)
     # aggregation of values
     v = self.value(x) # (B, T, C)
     out = wei @ v  # (B, T, C)
     return out
+  
+class MultiHeadAttention(nn.Module):
+  
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj = nn.Linear(head_size * num_heads, n_embed)
+    self.dropout = nn.Dropout(dropout)
     
+  def forward(self, x):
+    out = torch.cat([h(x) for h in self.heads], dim=-1) 
+    out = self.dropout(self.proj(out))
+    return out
   
 
-model = BigramLanguageModel()
+model = TransformerLanguageModel()
 m = model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
@@ -140,4 +209,4 @@ for iter in range(iterations):
 
 # generate from model
 context = torch.zeros((1,1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens=3000)[0].tolist()))
